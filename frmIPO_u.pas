b279b445c94fd21a0c9ab42cea292646b2996bf8
Breno-D <brenord@hotmail.com.br>
2026-04-09 21:03:32 -0300

    IPO

---
+++ frmIPO_u.pas
@@ -0,0 +1,34 @@
+unit frmIPO_u;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Edit;
+
+type
+  TForm1 = class(TForm)
+    edtInput: TEdit;
+    btnProcess: TButton;
+    edtOutput: TEdit;
+    procedure btnProcessClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.btnProcessClick(Sender: TObject);
+begin
+    edtOutput.text := edtInput.Text + ' Was Processed';
+end;
+
+end.
