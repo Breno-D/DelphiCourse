4bcfc023b10117584fa47a43437ae421402ad6f8
Breno-D <brenord@hotmail.com.br>
2026-04-18 17:58:33 -0300

    readfile

---
+++ frmRead_u.pas
@@ -0,0 +1,35 @@
+unit frmRead_u;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Edit,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    btnRead: TButton;
+    edtOutput: TEdit;
+    procedure btnReadClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.btnReadClick(Sender: TObject);
+begin
+edtOutPut.Text := '';
+
+edtOutPut.Text.LoadFromFile();
+end;
+
+end.
